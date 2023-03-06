83622a3babed2ccf41a0b05f5e505a8ca0a90220
Dannymane <daniel.yanko5@gmail.com>
2023-03-06 21:32:12 +0100

    Ccreted class user, Microsoft.Extensions.Identity.Stores hasn't installed

---
+++ SchoolRegister.Model/DataModels/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SchoolRegister.Model.DataModels
+{
+    internal class User : IdentityUser<int>
+    {   
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
